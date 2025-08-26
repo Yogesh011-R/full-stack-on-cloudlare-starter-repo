e9ad82d2043b4aad6f138da77bb196a1380313e5
Yogesh011-R <yogesh.kumar.rr5@gmail.com>
2025-08-26 17:11:51 +0530

    added workflow with ai checker and r2

--- apps/data-service/src/workflows/desitination-evalutaion-workflow.ts
+++ apps/data-service/src/workflows/desitination-evalutaion-workflow.ts
@@ -1,15 +1,46 @@
 import { WorkerEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers'
 import { collectDestinationInfo } from '@/helpers/browser-render'
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker'
+import { addEvaluation } from '@repo/data-ops/queries/evaluations'
+import { initDatabase } from '@repo/data-ops/database'
 
 export class DestinationEvaluationWorkflow extends WorkerEntrypoint<Env> {
   async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep): Promise<void> {
-
+    initDatabase(this.env.DB)
     const collectedData = await step.do("Collect rendered destination page data", async () => {
       return collectDestinationInfo(this.env, event.payload.destinationUrl)
     })
-    console.log(collectedData)
+
+    const aiResult = await step.do("AI Destination Status Evaluation", {
+      retries: {
+        limit: 0,
+        delay: 0
+      }
+    }, async () => {
+      return aiDestinationChecker(this.env, collectedData.bodyText)
+    })
 
 
+
+
+    const evaluationId = await step.do("Save evaluation to DB", async () => {
+      return addEvaluation({
+        linkId: event.payload.linkId,
+        accountId: event.payload.accountId,
+        destinationUrl: event.payload.destinationUrl,
+        status: aiResult.status,
+        reason: aiResult.statusReason,
+      })
+    })
+
+
+    await step.do("Backup destination page HTML data to R2", async () => {
+      const accountId = event.payload.accountId
+      const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`
+      const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`
+      await this.env.BUCKET.put(r2PathHtml, collectedData.html)
+      await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText)
+    })
   }
 
 }
